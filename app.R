2f4fddae68a015f2e5099df97080c951c76fc7e9
tylerhill122 <tylergenehill122@gmail.com>
2024-01-10 12:53:35 -0500

    Added managedarea map capability, with animated slider

--- app.R
+++ app.R
@@ -158,15 +158,34 @@ sample_map <- function(habitat="Oyster", pal){
   return(map %>% hideGroup(c("ORCP Boundaries")))
 }
 
+# leaflet map template
+create_map <- function(){
+  map <- leaflet() %>%
+    addTiles(group="Default") %>%
+    addProviderTiles(providers$CartoDB.PositronNoLabels, group = "Positron by CartoDB") %>%
+    addMapPane("background", zIndex = 400) %>%
+    addMapPane("foreground", zIndex = 500) %>%
+    addPolygons(data=orcp_shp, color="#4e809c", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.2,
+                group="ORCP Boundaries", options = pathOptions(pane = "background")) %>%
+    addLayersControl(
+      overlayGroups = c("ORCP Boundaries"),
+      baseGroups = c("Default", "Positron by CartoDB"),
+      options = layersControlOptions(collapsed = TRUE)) %>%
+  
+  return(map %>% hideGroup(c("ORCP Boundaries")))
+}
+
 summary_table <- function(data){
-  return(
-    data %>%
-      group_by(ParameterName) %>%
-      summarise(N_samples = n(),
-                mean = round(mean(ResultValue),2),
-                min = min(ResultValue),
-                max = max(ResultValue))
-  )
+  if(nrow(data)>0){
+    return(
+      data %>%
+        group_by(ParameterName) %>%
+        summarise(N_samples = n(),
+                  mean = round(mean(ResultValue),2),
+                  min = min(ResultValue),
+                  max = max(ResultValue))
+    )
+  }
 }
 
 plot_parameters <- function(data, parameter){
@@ -174,17 +193,63 @@ plot_parameters <- function(data, parameter){
     filter(ParameterName==parameter) %>%
     group_by(Year) %>%
     summarise(mean=mean(ResultValue))
+  if (nrow(plot_data)>0){
+    x_max <- max(plot_data$Year)
+    x_min <- min(plot_data$Year)
+    
+    plot <- ggplot(plot_data, aes(x=Year, y=mean)) +
+      geom_bar(stat = "identity", fill="#4472C4") +
+      labs(title=paste0("Mean ", parameter, " by year"),
+           x="Year",
+           y=paste0("Mean ", parameter)) +
+      scale_x_continuous(breaks=seq(x_max, x_min, -1)) +
+      # coord_cartesian(xlim=c(x_min, x_max)) +
+      plot_theme
+    return(plot)
+  }
   
-  plot <- ggplot(plot_data, aes(x=Year, y=mean)) +
-    geom_bar(stat = "identity", fill="#4472C4") +
-    labs(title=paste0("Mean ", parameter, " by year"),
-         x="Year",
-         y=paste0("Mean ", parameter)) +
-    scale_x_continuous(limits=c(min(plot_data$Year), max(plot_data$Year)),
-                       breaks=seq(max(plot_data$Year), min(plot_data$Year), -1)) +
-    plot_theme
+}
+
+create_ma_map <- function(data, ma, ret){
+  # locate shape file for given MA
+  ma_shape <- find_shape(ma)
   
-  return(plot)
+  # get coordinates to set zoom level
+  shape_coordinates <- get_shape_coordinates(ma_shape)
+  
+  # create list of LocationIDs with habitat data for each MA
+  locations <- data %>% filter(ManagedAreaName==ma) %>% 
+    distinct(LocationID)
+  
+  # Number of data points for each sample location (point size)
+  ma_data <- data %>% filter(ManagedAreaName==ma) %>%
+    group_by(LocationID, Year) %>%
+    summarise(n_data = n(), .groups="keep")
+  
+  # Filtering samplelocation coordinates for each MA
+  df <- point %>% filter(LocationID %in% locations$LocationID)
+  
+  # Merge into single dataframe
+  combined_df <- merge(x=ma_data, y=df, by.x="LocationID", by.y="LocationID")
+  
+  # set palette
+  pal <- colorFactor("plasma", combined_df$ProgramID)
+  
+  map <- leaflet() %>%
+    addTiles() %>%
+    addPolygons(data=ma_shape, color="#4e809c", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.2) %>%
+    setMaxBounds(lng1=shape_coordinates$xmin,
+                 lat1=shape_coordinates$ymin,
+                 lng2=shape_coordinates$xmax,
+                 lat2=shape_coordinates$ymax) %>%
+    addLegend(data = combined_df, pal=pal, position="bottomright", values=~ProgramID,
+              labFormat=labelFormat(prefix="Program "))
+  
+  if (ret == "map"){
+    return(map)
+  } else if (ret =="data"){
+    return(combined_df)
+  }
 }
 
 habitats <- c("Oyster","Submerged Aquatic Vegetation")
@@ -206,22 +271,43 @@ ui <- fluidPage(
     column(4, 
            leafletOutput("leaflet_map"))
   ),
-  titlePanel("By Program"),
-  fluidRow(
-    column(4,
-           wellPanel(
-             selectInput(inputId = "programSelect",
-                         label = "Select Program to Preview Summary Statistics",
-                         choices = "Choose a Program"),
-             tableOutput("sum_table"),
-             selectInput(inputId = "parameterSelect",
-                         label = "Select Parameter to Plot",
-                         choices = "Choose a Parameter")
+  tabsetPanel(
+    tabPanel("By Program",
+             fluidRow(
+               column(4,
+                      wellPanel(
+                        selectInput(inputId = "programSelect",
+                                    label = "Select Program to Preview Summary Statistics",
+                                    choices = "Choose a Program"),
+                        tableOutput("sum_table"),
+                        selectInput(inputId = "parameterSelect",
+                                    label = "Select Parameter to Plot",
+                                    choices = "Choose a Parameter")
+                      )
+               ),
+               column(8,
+                      plotOutput("parameter_plot"))
              )
-           ),
-    column(8,
-           plotOutput("parameter_plot"))
-  )
+             ),
+    tabPanel("By Managed Area",
+             fluidRow(
+               column(3,
+                      selectInput("managedArea", "Select Managed Area to View",
+                                  choices = c("Biscayne Bay Aquatic Preserve", "Estero Bay Aquatic Preserve"), 
+                                  "Biscayne Bay Aquatic Preserve")),
+               column(3,
+                      sliderInput("yearslider", "Time:",
+                                  min = 1999,
+                                  max = 2022,
+                                  value = 1999,
+                                  step = 1,
+                                  sep = "",
+                                  animate = animationOptions(interval = 1000, loop=TRUE))),
+               column(6, "hey")),
+             fluidRow(
+               leafletOutput("mapAct")
+             ))
+  ),
 )
 
 server <- function(input, output, session){
@@ -246,17 +332,72 @@ server <- function(input, output, session){
     data()[ProgramID==input$programSelect, ]
   })
   
+  data_by_ma <- reactive({
+    from <- input$yearslider
+    till <- input$yearslider
+    create_ma_map(data(), input$managedArea, "data") %>%
+      filter(Year >= from & Year <= till)
+  })
+  
   output$param_plot <- renderPlot(program_param_plot(data=data(), ret="plot"))
   
   output$program_plot <- renderPlot(program_years_plot(data=data(), pal()))
   
-  output$leaflet_map <- renderLeaflet(sample_map(habitat=input$habitatSelect, pal()))
+  output$leaflet_map <- renderLeaflet(create_map())
   
   output$output_table <- renderTable(head(data_by_program()))
   
   output$sum_table <- renderTable(summary_table(data_by_program()))
   
   output$parameter_plot <- renderPlot(plot_parameters(data=data_by_program(), parameter=input$parameterSelect))
+  
+  output$mapAct <- renderLeaflet({
+    create_ma_map(data(), input$managedArea, "map")
+  })
+  
+  observe({
+    
+    data <- data_by_ma()
+    
+    colorPalette <- create_ma_map(data(), input$managedArea, "data")
+    
+    rad <- sqrt(data$n_data)
+    
+    updateSliderInput(session = session, "yearslider",
+                      min = min(colorPalette$Year),
+                      max = max(colorPalette$Year),
+                      step = 1)
+    
+    pal <- colorFactor("plasma", colorPalette$ProgramID)
+    
+    leafletProxy("mapAct", data=data) %>%
+      clearMarkers() %>%
+      addCircleMarkers(lat=~Latitude_D, lng=~Longitude_, color=~pal(ProgramID),
+                       weight=1, radius=rad, fillOpacity=0.4,
+                       label = paste0("ProgramID: ",data$ProgramID, " | LocationID: ", data$LocationID))
+    
+  })
+  
+  # map observe
+  observe({
+    
+    if (input$habitatSelect == "Oyster"){
+      data <- oyster_map_data
+    } else if (input$habitatSelect == "Submerged Aquatic Vegetation"){
+      data <- sav_map_data
+    }
+    
+    rad <- sqrt(data$n_data)/5
+    
+    leafletProxy("leaflet_map", data = data) %>%
+      clearMarkers() %>%
+      addCircleMarkers(lat=~Latitude_D, lng=~Longitude_, color=~pal()(ProgramID),
+                       weight=1, radius=rad, fillOpacity=0.4,
+                       popup = ~popup,
+                       label = ~label,
+                       options = pathOptions(pane = "foreground")) %>% 
+      hideGroup(c("ORCP Boundaries"))
+  })
   
   observe({
     included_programs <- sort(as.integer(unique(data()$ProgramID)))
@@ -276,6 +417,18 @@ server <- function(input, output, session){
                       label = "Select Parameter to Plot", 
                       choices = included_parameters,
                       selected = head(included_parameters,1))
+    
+  })
+  
+  observe({
+    
+    managed_areas <- sort(unique(data()$ManagedAreaName))
+    
+    updateSelectInput(session = session, 
+                      inputId = "managedArea", 
+                      label = "Select Managed Area to Explore", 
+                      choices = managed_areas,
+                      selected = input$managedArea)
     
   })
 }
